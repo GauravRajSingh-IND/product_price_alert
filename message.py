bbae9f6a389edc97725b89699ab1c560e14e2173
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-29 12:29:54 +0530

    create a function to send whatsapp message

--- message.py
+++ message.py
@@ -18,20 +18,39 @@ class SendMessage:
         self.client = self.set_client()
         self.is_client_set = False
 
+        self.is_message_send = False
+
     def set_client(self, retries:int=3):
         """
         This function set twilio client object.
         :param retries: number of retries.
         :return:
         """
-
         for retrie in range(retries):
+
+            # Set client object using sid and token.
             client = Client(self.sid, self.token)
 
+            # check if client is set or not.
             if client:
                 self.is_client_set= True
                 return client
             else:
                 raise f"Client not set properly on try: {retrie}"
 
-whatsapp = SendMessage("123456")
+    def send_message(self, message= "Hello Testing", retries:int = 3):
+        """
+        This function send whatsapp message to user.
+        :return:
+        """
+        for retry in range(retries):
+            message = self.client.messages.create(
+                body=message,
+                from_=self.company_number,
+                to = f"whatsapp:{self.user_phone_number}"
+            )
+
+            if message.sid:
+                self.is_message_send = True
+                return None
+
